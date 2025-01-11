d6fe69a313f84c7ce0f67bcaa6f7fbcc0c75603c
Nouhad122 <nouhadalhallab122@gmail.com>
2025-01-11 18:50:55 +0300

    fetching data to a firebase server and adding notifications

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,27 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartOpened);
+  const cart = useSelector(state => state.cart.items);
+
+  useEffect(() =>{
+    const fetchCartItems = async () =>{
+      try{
+        const response = await fetch("https://react-http-3f0d9-default-rtdb.firebaseio.com/cart.json", {
+                method: "PUT",
+                body: JSON.stringify(cart)
+              });
+
+        if(!response.ok){
+        throw new Error("Something went wrong! Cannot fetch data.");
+      }
+      }
+      catch(error){
+        //error
+      }
+    }
+    fetchCartItems();
+  },[cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
